636d6f485a65563ed58431d82156cf434209b25d
Lillian Yang <lyang@lcs.on.ca>
2022-11-17 11:53:54 -0500

    Got a diagonal to show up

--- CustomShapesExamples/ContentView.swift
+++ CustomShapesExamples/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct Diagonal : Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //the path that describes the shape
+        var path = Path()
+        
+        //Here is where we describe the shape
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        //Send back (return) the path that describes the shape
+        return path
+    }
+    
+    
+}
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Diagonal()
+            .stroke()
+        //line
+        
     }
 }
 
