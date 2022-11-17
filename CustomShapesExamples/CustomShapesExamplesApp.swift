0085e4ccca40c193ac3e1b7389222a70159ec167
Lillian Yang <lyang@lcs.on.ca>
2022-11-17 11:41:39 -0500

    Initial Commit

---
+++ CustomShapesExamples/CustomShapesExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesExamplesApp.swift
+//  CustomShapesExamples
+//
+//  Created by Lillian Yang on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
